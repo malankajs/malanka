45538b545839e348b08a750f702df9db2da85a1e
Evgeny Shpilevsky <ftdebugger@yandex-team.ru>
2016-07-10 13:53:14 +0300

    Fix styles optimizer specs

--- test/StylesOptimizer.spec.js
+++ test/StylesOptimizer.spec.js
@@ -23,37 +23,37 @@ describe('StylesOptimizer', function () {
     it('transform two classes', function () {
         let result = compiler.compileString('<div class="test1 test2"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__mergeStrings([context.styles.test1," ",context.styles.test2], context).join(\'\')}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__join(__mergeStrings([context.styles.test1," ",context.styles.test2], context))}})}');
     });
 
     it('transform class with interpolation', function () {
         let result = compiler.compileString('<div class="test1 {{test}} test2"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__mergeStrings([context.styles.test1," ",context.test," ",context.styles.test2], context).join(\'\')}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__join(__mergeStrings([context.styles.test1," ",context.test," ",context.styles.test2], context))}})}');
     });
 
     it('transform class with block helpers', function () {
         let result = compiler.compileString('<div class="test1 {{#if test}} test2{{/if}}"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",context.styles.test2], context).join(\'\')}})], context).join(\'\')}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__join(__mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __join(__mergeStrings([" ",context.styles.test2], context))}})], context))}})}');
     });
 
     it('transform class with block helpers', function () {
         let result = compiler.compileString('<div class="test1 {{#if test}} {{v1}}{{/if}}"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",context.v1], context)}})], context).join(\'\')}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__join(__mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",context.v1], context)}})], context))}})}');
     });
 
     it('transform class with block helpers', function () {
         let result = compiler.compileString('<div class="test1 {{#if test}} {{@v1}}{{/if}}"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":(function(v0){return v0.mutate(function(){return __mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",v0], context)}})], context).join(\'\')})})(context.proxy("v1"))}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":(function(v0){return v0.mutate(function(){return __join(__mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",v0], context)}})], context))})})(context.proxy("v1"))}})}');
     });
 
     it('transform class with helper and string params', function () {
         let result = compiler.compileString('<div class="test1 {{test "abc"}}"></div>').split('\n').pop();
 
-        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":(function(v0){return v0.mutate(function(){return __mergeStrings([context.styles.test1," ",context.if.call(context,context.test,{"hash":{},"isString":true,"content":function(){return __mergeStrings([" ",v0], context)}})], context).join(\'\')})})(context.proxy("v1"))}})}');
+        expect(result).to.equal('module.exports = function(context){return new Component({"attributes":{"class":__join(__mergeStrings([context.styles.test1," ",context.test.call(context,"abc",{"hash":{},"isString":true})], context))}})}');
     });
 
 });
